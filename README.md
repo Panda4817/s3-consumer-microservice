8835a9d6d5d1d8d7a833c5ef6442b99a394f5419
Kanta Munton <kanta.munton@bjss.com>
2022-03-19 20:28:11 +0000

    update readme

--- README.md
+++ README.md
@@ -7,5 +7,16 @@ and uses it to calculate a response for the marvel hero endpoint (gives a marvel
 - It has an endpoint that can be called to fetch new file from S3 bucket 
 and this will be invoked by an AWS lambda function when new file is uploaded to S3 bucket.
 
+## AWS usage
+![aws.png](aws.png)
+
+- [S3 bucket file uploader UI project](https://github.com/Panda4817/s3-file-uploader-ui)
+
+### API Endpoint Example
+```
+curl https://s3-consumer-microservice.herokuapp.com/marvel\?attribute1\=wolf
+wolverine
+```
+
 
 
--- src/main/java/dev/kmunton/mappings/consumer/api/ConsumerContract.java
+++ src/main/java/dev/kmunton/mappings/consumer/api/ConsumerContract.java
@@ -6,14 +6,12 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 
 import java.io.IOException;
 
 import static io.swagger.v3.oas.annotations.enums.ParameterIn.QUERY;
 
 @Tag(name = "Mappings Consumer")
-@Validated
 public interface ConsumerContract {
 
     @Operation(summary = "Retrieve a marvel hero based on attributes provided")
@@ -22,13 +20,13 @@ public interface ConsumerContract {
             @ApiResponse(responseCode = "400", description = "The request was malformed"),
             @ApiResponse(responseCode = "500", description = "Marvel hero could not be retrieved due to an unknown error")})
     ResponseEntity<String> getMarvelHero(@Parameter(name = "attribute1", in= QUERY, required = true, example = "red") String attribute1,
-                                         @Parameter(name = "attribute2", in= QUERY, required = false, example = "hammer") String attribute2);
+                                         @Parameter(name = "attribute2", in= QUERY, required = false) String attribute2);
 
     @Operation(summary = "Refresh Mappings")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "The request completed successfully"),
             @ApiResponse(responseCode = "400", description = "The request was malformed"),
             @ApiResponse(responseCode = "500", description = "Something went wrong")})
-    ResponseEntity<Void> getNewMappings(@Parameter(name = "key", in= QUERY, required = true, example = "mappings_file_v1_0.csv") String key) throws IOException;
+    ResponseEntity<Void> getNewMappings(@Parameter(name = "key", in= QUERY, required = true, example = "test.csv") String key) throws IOException;
 }
 
